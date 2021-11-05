6bd35c99a91622392275f333216d7fb1ea8e5025
aprilliya34 <rismadewiaprilliya@gmail.com>
2021-11-06 05:20:07 +0700

    Intent_RismaDewiA
    Nama : Risma Dewi Aprilliya
    Nim : 20.52.0010
    Tugas : PPB KE 6 - INTENT

---
+++ app/src/main/java/com/risma/intent_rismadewia/MoveWithDeta.kt
@@ -0,0 +1,44 @@
+package com.risma.intent_rismadewia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MoveWithDeta : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvDataReceived: TextView
+    private lateinit var btnBackToMainPage: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_deta)
+
+        tvDataReceived = findViewById(R.id.tv_data_received)
+        DataReceived()
+        btnBackToMainPage = findViewById(R.id.btn_backto_main_page)
+        btnBackToMainPage.setOnClickListener(this)
+    }
+
+    private fun DataReceived() {
+        val bundle = intent.extras
+        val nama = bundle?.getString("Nama")
+        val alamat = bundle?.getString("Alamat")
+
+        tvDataReceived.text = nama + " " + alamat
+    }
+
+    override fun onClick(v: View?) {
+        if (v!=null){
+            when(v.id){
+                R.id.btn_backto_main_page -> {
+                    val intent=Intent(this, BackToMainPage::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+}
+
+
